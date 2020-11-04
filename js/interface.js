0326c89a293d6eecc0360fd560df8cfc904216a3
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2020-11-04 16:13:17 +0200

    add access rules for chart column

--- js/interface.js
+++ js/interface.js
@@ -60,6 +60,15 @@ var dsQueryData = {
       }
     ]
   },
+  accessRules: [
+    {
+      allow: 'all',
+      enabled: true,
+      type: [
+        'select'
+      ]
+    }
+  ],
   result: data.dataSourceQuery
 };
 
